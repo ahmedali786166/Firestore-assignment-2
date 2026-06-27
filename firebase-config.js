094c4563bfc6ca0505445f05e1c48d0b7aaed325
Ahmed Ali <ahmedalisohu3@gmail.com>
2026-06-27 20:32:56 +0500

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,13 +4,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBVhl079jUx4__fq8B_ff_YFu_MNpoH0SI",
-  authDomain: "assignment-84515.firebaseapp.com",
-  projectId: "assignment-84515",
-  storageBucket: "assignment-84515.firebasestorage.app",
-  messagingSenderId: "1043911319431",
-  appId: "1:1043911319431:web:ae7fc6af7b391e14bb3d22",
-  measurementId: "G-Y2RFN174SB"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
